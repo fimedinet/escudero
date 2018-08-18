72e72a0868ee3974975c4bd98f39e3849a2bd941
Ariel Vallese <alariva@gmail.com>
2018-08-18 20:52:35 -0300

    Add BMI Type on WHO classification

--- src/Tools/BMI/BMILevel.php
+++ src/Tools/BMI/BMILevel.php
@@ -12,6 +12,16 @@ class BMILevel
     const LEVEL_HIGH = 'HIGH';
     const LEVEL_VERY_HIGH = 'VERYHIGH';
 
+    const TYPE_SEVERE_THINNESS = -3;
+    const TYPE_MODERATE_THINNESS = -2;
+    const TYPE_MILD_THINNESS = -1;
+    const TYPE_REGULAR = 0;
+    const TYPE_OVERWEIGHT = 1;
+    const TYPE_PRE_OBESE = 2;
+    const TYPE_OBESE_GRADE_I = 3;
+    const TYPE_OBESE_GRADE_II = 4;
+    const TYPE_OBESE_GRADE_III = 5;
+
     public static function category(float $bmi)
     {
         if ($bmi <= 18.5) {
@@ -27,5 +37,44 @@ class BMILevel
         }
 
         return self::LEVEL_VERY_HIGH;
+    }
+
+    public static function classification(float $bmi) : int
+    {
+        if ($bmi <= 16.00) {
+            return self::TYPE_SEVERE_THINNESS;
+        }
+
+        if ($bmi <= 16.99) {
+            return self::TYPE_MODERATE_THINNESS;
+        }
+
+        if ($bmi <= 18.49) {
+            return self::TYPE_MILD_THINNESS;
+        }
+
+        if ($bmi <= 24.99) {
+            return self::TYPE_REGULAR;
+        }
+
+        if ($bmi <= 27.49) {
+            return self::TYPE_OVERWEIGHT;
+        }
+
+        if ($bmi <= 29.99) {
+            return self::TYPE_PRE_OBESE;
+        }
+
+        if ($bmi <= 34.99) {
+            return self::TYPE_OBESE_GRADE_I;
+        }
+
+        if ($bmi <= 39.99) {
+            return self::TYPE_OBESE_GRADE_II;
+        }
+
+        if ($bmi >= 40) {
+            return self::TYPE_OBESE_GRADE_III;
+        }
     }
 }
--- tests/Tools/BMI/BMILevelTest.php
+++ tests/Tools/BMI/BMILevelTest.php
@@ -27,4 +27,32 @@ class BMILevelTest extends BaseTestCase
             $this->assertEquals($category, $categoryCheck);
         }
     }
+
+    /**
+     * Test it returns a valid WHO classification for BMI type
+     *
+     * @return void
+     */
+    public function test_it_returns_a_valid_who_classification_for_bmi_type()
+    {
+        // Sample bmi => expected type
+        // Key must be a string later converted to float
+        $testMatrix = [
+            "15" => BMILevel::TYPE_SEVERE_THINNESS,
+            "16.5" => BMILevel::TYPE_MODERATE_THINNESS,
+            "18" => BMILevel::TYPE_MILD_THINNESS,
+            "24" => BMILevel::TYPE_REGULAR,
+            "27" => BMILevel::TYPE_OVERWEIGHT,
+            "29" => BMILevel::TYPE_PRE_OBESE,
+            "34" => BMILevel::TYPE_OBESE_GRADE_I,
+            "39" => BMILevel::TYPE_OBESE_GRADE_II,
+            "41" => BMILevel::TYPE_OBESE_GRADE_III,
+        ];
+
+        foreach ($testMatrix as $bmi => $categoryCheck) {
+            $type = BMILevel::classification(floatval($bmi));
+
+            $this->assertEquals($type, $categoryCheck);
+        }
+    }
 }
