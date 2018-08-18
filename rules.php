49faea075f6d8b23f6f7b5ac7105a85adce7450f
Ariel Vallese <alariva@gmail.com>
2018-08-18 07:44:15 -0300

    Remove unused support files

--- rules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: fimedinet, escudero, fimedinet, escudero
-return [
-    'src/MyPackage.php' => 'src/escudero.php',
-    'config/mypackage.php' => 'config/escudero.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/escudero.php',
-    'src/MyPackageServiceProvider.php' => 'src/escuderoServiceProvider.php',
-];
