f42b96279a3c63e684fa96ce45bccfa7c2f79c20
Ariel Vallese <alariva@gmail.com>
2018-08-26 04:47:26 -0300

    Add Wrist Circumference input sanitizer

---
+++ src/Tools/WrC/Input/WristCircumferenceInput.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FimediNET\Escudero\Tools\WrC\Input;
+
+class WristCircumferenceInput
+{
+    /**
+     * Sanitize a Wrist Circumference value expressed in cm or mm to mm
+     * 
+     * @param  mixed $value Expression for Wrist Circumference in cm or mm
+     * @return int          Converted Wrist Circumference into mm
+     */
+    public static function sanitize($value)
+    {
+        $sanitizedValue = $value;
+
+        $sanitizedValue = str_replace(',', '.', $sanitizedValue);
+        $sanitizedValue = str_replace('..', '.', $sanitizedValue);
+        $sanitizedValue = str_replace('.', '', $sanitizedValue);
+
+        $sanitizedValue = floatval($sanitizedValue);
+
+        if ($sanitizedValue < 10) {
+            $sanitizedValue *= 10;
+        }
+
+        if ($sanitizedValue < 40) {
+            $sanitizedValue *= 10;
+        }
+
+        return $sanitizedValue;
+    }
+}
---
+++ tests/Tools/WrC/Input/WristCircumferenceInputTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Tools\WrC\Input;
+
+use FimediNET\Escudero\Tools\WrC\Input\WristCircumferenceInput;
+use Tests\BaseTestCase;
+
+class WristCircumferenceInputTest extends BaseTestCase
+{
+    /**
+     * Test it accepts Wrist Circumference input in centimeters or milimeters
+     *
+     * @dataProvider provider
+     * 
+     * @return void
+     */
+    public function test_it_accepts_wrist_circumference_input_in_centimeters_or_milimeters($input, $expected)
+    {
+        $sanitizedWrC = WristCircumferenceInput::sanitize($input);
+
+        $this->assertEquals($sanitizedWrC, $expected);
+    }
+
+    /**
+     * Provides [userInput, expectedOutput] for test case
+     * 
+     * @return array [userInputWrC, expectedOutputWrC]
+     */
+    public function provider()
+    {
+        return [
+            ['9.5' , 95],
+            ['9,5' , 95],
+            ['16'  , 160],
+            ['16.5', 165],
+            ['165' , 165],
+            ['200' , 200],
+            ['20'  , 200],
+            ['350' , 350],
+            ['400' , 400],
+        ];
+    }
+}
